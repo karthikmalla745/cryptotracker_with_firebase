be2cce55a366626d2a297024bdad1c19633b221f
karthikmalla745 <82028779+karthikmalla745@users.noreply.github.com>
2022-06-05 12:39:51 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmf5VPxGQBSJ-2uXi7JcfWtsL28np7eBs",
+  authDomain: "crypto-hunter-f120c.firebaseapp.com",
+  projectId: "crypto-hunter-f120c",
+  storageBucket: "crypto-hunter-f120c.appspot.com",
+  messagingSenderId: "166693229977",
+  appId: "1:166693229977:web:3a038088f823d3f41caf08"
+};
+export default firebaseConfig;
